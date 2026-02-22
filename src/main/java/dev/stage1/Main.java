64930afd9ce7d27663d9af39545d5872fb62cfbc
silvia <ciubucsilviag@gmail.com>
2026-02-22 15:10:00 +0100

    first commit

---
+++ src/main/java/dev/stage1/Main.java
@@ -0,0 +1,11 @@
+package dev.stage1;
+
+public class Main {
+    public static void main(String[] args) {
+        String botName = "Aid";
+        String birthYear = "2023";
+
+        System.out.printf("Hello! My name is %s.%n", botName);
+        System.out.printf("I was created in %s.%n", birthYear);
+    }
+}
